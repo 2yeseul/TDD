3638659a0f47a2576fffc6cad4e41224408a63e9
이예슬 <20152917@sungshin.ac.kr>
2022-03-05 02:02:16 +0900

    Create Money Object and actions

--- src/main/kotlin/com/example/tdd/ch01/Money.kt
+++ src/main/kotlin/com/example/tdd/ch01/Money.kt
@@ -4,8 +4,19 @@ class Money(
     private val stocks: String,
     private val amount: Int,
     private val price: Int,
+    private val currency: Currency? = Currency.USD
 ) {
-    fun getTotalStockPrice(): Int {
-        return this.amount * this.price
+    fun getTotalStockPrice(): Double {
+        return this.amount * (this.price / (this.currency?.exchangeRate ?: 1.0))
     }
+
+    companion object {
+        fun Money.add(money: Money): Double {
+            return this.getTotalStockPrice() + money.getTotalStockPrice()
+        }
+    }
+}
+
+enum class Currency(val exchangeRate: Double) {
+    USD(1.0), CHF(1.5)
 }
