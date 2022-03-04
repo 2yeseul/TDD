6cbdd99d6814bf5a9ce55ef336295e60a07ed2d0
이예슬 <20152917@sungshin.ac.kr>
2022-03-05 02:02:16 +0900

    Create Money Object and actions

--- src/test/kotlin/com/example/tdd/ch01/MoneyTest.kt
+++ src/test/kotlin/com/example/tdd/ch01/MoneyTest.kt
@@ -1,5 +1,6 @@
 package com.example.tdd.ch01
 
+import com.example.tdd.ch01.Money.Companion.add
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -15,6 +16,22 @@ class MoneyTest {
         val price = 25
 
         val money = Money("IBM", amount, price)
-        assertThat(money.getTotalStockPrice()).isEqualTo(amount * price)
+        assertThat(money.getTotalStockPrice()).isEqualTo(amount * price.toDouble())
+    }
+
+    @Test
+    @DisplayName("통화가 다른 두 금액을 더해서 주어진 환율에 맞게 변한 금액을 결과로 얻을 수 있어야함")
+    fun testAdjustCurrencies() {
+        val ibmAmount = 1000
+        val ibmPrice = 25
+
+        val ibm = Money("IBM", ibmAmount, ibmPrice, Currency.USD)
+
+        val novartisAmount = 400
+        val novartisPrice = 150
+        val novartis = Money("Novartis", novartisAmount, novartisPrice, Currency.CHF)
+
+        val result = ibm.add(novartis)
+        assertThat(result).isEqualTo(65000.toDouble())
     }
 }
